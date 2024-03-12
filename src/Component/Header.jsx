1e4750213f21c85eb5e008a8fdc61fb08ac0cb8e
Sumitsonikaju <sumitsoni79059@gmail.com>
2024-03-12 13:30:43 +0530

    final

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { adduser, removeUser } from "../utils/userSlice";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const [isOpen, setisOpen] = useState(false);
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -44,6 +46,10 @@ const Header = () => {
   const isDisplayUserInfo = location.pathname === "/displayUser";
   const isLogin = location.pathname === "/";
 
+  const handleToggle = () => {
+    setisOpen(!isOpen);
+  }
+
   return (
     <>
       <nav className="flex justify-between fixed w-full items-center px-10 z-40 min-h-[8vh] bg-white shadow-md">
@@ -59,7 +65,7 @@ const Header = () => {
             placeholder="search your location....."
           />
         )}
-        <ul className="flex space-x-4 items-center">
+        <ul className="flex space-x-4 items-center max-md:hidden">
         {user && <h2>I am, <span className="font-bold text-blue-500">{user.displayName}</span></h2>}
 
 
@@ -107,7 +113,59 @@ const Header = () => {
             </button>
           )}
         </ul>
+        <FaBars onClick={handleToggle} className="hidden text-3xl cursor-pointer ml-4 max-md:block" />
       </nav>
+
+
+
+      <div>
+      <ul className={` hidden ${isOpen ? "max-md:block" : "max-md:hidden"} pt-[100px] text-center bg-gray-200 pb-5 shadow-md `}>
+        {user && <h2>I am, <span className="font-bold text-blue-500">{user.displayName}</span></h2>}
+          {!user && !isLogin && (
+            <>
+              <Link to={"/"}>
+                <button className="font-bold cursor-pointer shadow-md">Login</button>
+              </Link>
+            </>
+            )}
+            {!user && (
+            <>
+              <Link to={"/signup"}>
+                {" "}
+                <button className="border px-3 py-2 bg-purple-400 text-white font-bold rounded-md">
+                  Signup
+                </button>
+              </Link>
+            </>
+            )}
+          
+          {user && !isDisplayUserInfo && (
+            <Link to={"/displayUser"}>
+              <button className="font-bold cursor-pointer block mx-auto">
+                Listed Roommates
+              </button>
+            </Link>
+          )}
+          {user && !isCreatePage && (
+            <Link to={"/Create"}>
+              <button className="font-bold cursor-pointer block mx-auto">Create</button>
+            </Link>
+          )}
+          {user && !isHomePage && (
+            <Link to={"/mainPage"}>
+              <button className="font-bold cursor-pointer block mx-auto">Home</button>
+            </Link>
+          )}
+          {user && (
+            <button
+              className="border px-3 py-2 bg-purple-400 text-white font-bold rounded-md block mx-auto"
+              onClick={handeLogout}
+            >
+              Logout
+            </button>
+          )}
+        </ul>
+      </div>
     </>
   );
 };
