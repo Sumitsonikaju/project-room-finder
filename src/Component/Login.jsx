0292ab5c3d0374e741d46b83521a87e7326bf4c3
Sumitsonikaju <sumitsoni79059@gmail.com>
2024-03-29 00:19:42 +0530

    Google Login button

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -100,12 +100,20 @@ const Login = () => {
           >
             Login
           </button>
-          <button
-            onClick={handleLoginWithGoogle}
-            className="border px-2 py-1 w-full mt-4 cursor-pointer bg-purple-400 text-white font-bold "
+          
+
+          <button className="px-4 py-2 mt-4 w-full pl-[30%] border flex gap-2 cursor-pointer  border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+          onClick={handleLoginWithGoogle}
           >
-            Login with Google
+            <img
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              loading="lazy"
+              alt="google logo"
+            />
+            <span className="text-black">Login with Google</span>
           </button>
+
           <p className="text-center font-bold mt-4">
             Don't have an Account ?{" "}
             <Link className="hover:underline text-blue-500" to={"/signup"}>
